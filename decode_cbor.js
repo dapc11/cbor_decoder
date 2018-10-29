43eb551ee9b5ea0d696ace66ae132f00f9f012be
Daniel Pettersson <daniel.pettersson@knowit.se>
2018-10-29 08:50:32 +0100

    Updated decoder to match Samtrafiken format of the Mobile Ticket Bundle

--- decode_cbor.js
+++ decode_cbor.js
@@ -2,16 +2,20 @@ const cbor = require('borc');
 const base64url = require('base64url');
 
 function printHex(buffer) {
-    console.log(buffer.toString('hex').match(/../g).join(' '));
+    return buffer.toString('hex').match(/../g).join(' ');
 }
 
 const args = process.argv;
 let mtb = base64url.toBuffer(args[2]);
-console.log("decoded base64url input:");
-printHex(mtb);
+console.log("decoded base64url encoded input:\n" + printHex(mtb));
 let decoded = cbor.decodeFirst(mtb);
-console.log("p:");
-printHex(decoded['p']);
+console.log("p:\n" + printHex(decoded['p']));
 decoded = cbor.decode(decoded['p']);
-decoded = cbor.decode(decoded[1]);
-console.log("human readable:\n" + JSON.stringify(decoded, null, 2));
+
+let headers = cbor.decode(decoded[0]);
+let payload = cbor.decode(decoded[1]);
+let signature = decoded[2];
+
+console.log("headers:\n" + JSON.stringify(headers, null, 2));
+console.log("payload:\n" + JSON.stringify(payload, null, 2));
+console.log("signature:\n" + printHex(signature));
